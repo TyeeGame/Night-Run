336dd79a659fc4a93dc555095b3f52201b953f51
Jerry Zhang <jerzhang414@gmail.com>
2025-08-08 01:14:00 -0700

    Initial commit of my Godot project

---
+++ player.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+var spawn_point = Vector2(72, 320)
+const SPEED = 250.0
+const JUMP_VELOCITY = -350.0
+
+
+func _ready(): spawn_point = position # This assumes the player starts at the correct position in the editor
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+func _on_death_zone_body_entered(body: Node2D) -> void:
+	if body == self:
+		print("Player died! Respawning...")
+		position = spawn_point
